ae07908164f27e48b33823d6d466a6e4255c6e0a
Andrew Shiels <andrew.shiels@adaptcentre.ie>
2018-08-21 14:19:05 +0000

    Update 'canvas2/canvas2/settings.py'

--- canvas2/canvas2/settings.py
+++ canvas2/canvas2/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
-    'django_extensions',
+    #'django_extensions',
 ]
 
 MIDDLEWARE = [
